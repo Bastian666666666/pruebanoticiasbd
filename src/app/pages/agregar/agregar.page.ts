8011d6dd2f55577963e1464150f7c01c33e159a3
Bastián Rabelo <ba.rabelo@duocuc.cl>
2024-06-15 22:18:27 -0400

    Terminado impresiciones
    , no funciona bien

--- src/app/pages/agregar/agregar.page.ts
+++ src/app/pages/agregar/agregar.page.ts
@@ -1,4 +1,6 @@
 import { Component, OnInit } from '@angular/core';
+import { Router } from '@angular/router';
+import { DbserviceService } from 'src/app/services/dbservice.service';
 
 @Component({
   selector: 'app-agregar',
@@ -7,9 +9,18 @@ import { Component, OnInit } from '@angular/core';
 })
 export class AgregarPage implements OnInit {
 
-  constructor() { }
+  tituloNoticia = "";
+  textoNoticia = "";
+  constructor(private dbservice: DbserviceService, private router: Router) { }
+
+  guardar() {
+    this.dbservice.addNoticia(this.tituloNoticia,this.textoNoticia);
+    this.dbservice.presentToast("Noticia Agregada");
+    this.router.navigate(['/home']);
+  }
 
   ngOnInit() {
   }
 
 }
+
