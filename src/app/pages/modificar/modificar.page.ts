3e2e94f980a1c9542c91aabe7fd3edf44dd53aca
Bastián Rabelo <ba.rabelo@duocuc.cl>
2024-06-15 22:18:32 -0400

    22

--- src/app/pages/modificar/modificar.page.ts
+++ src/app/pages/modificar/modificar.page.ts
@@ -1,4 +1,6 @@
 import { Component, OnInit } from '@angular/core';
+import { ActivatedRoute, Router } from '@angular/router';
+import { DbserviceService } from 'src/app/services/dbservice.service';
 
 @Component({
   selector: 'app-modificar',
@@ -6,10 +8,25 @@ import { Component, OnInit } from '@angular/core';
   styleUrls: ['./modificar.page.scss'],
 })
 export class ModificarPage implements OnInit {
+  idNoticia!: number;
+  tituloNoticia = "";
+  textoNoticia = "";
+  constructor(private router: Router, private activedroute: ActivatedRoute, private dbservice: DbserviceService) {
+    this.activedroute.queryParams.subscribe(param => {
+      if (this.router.getCurrentNavigation()?.extras.state) {
+        this.idNoticia = this.router.getCurrentNavigation()!.extras.state!['idEnviado'];
+        this.tituloNoticia = this.router.getCurrentNavigation()?.extras?.state?.['tituloEnviado'];
+        this.textoNoticia = this.router.getCurrentNavigation()?.extras?.state?.['textoEnviado'];
+      }
+    })
+  }
 
-  constructor() { }
+  editar() {
+    this.dbservice.updateNoticia(this.idNoticia, this.tituloNoticia, this.textoNoticia);
+    this.dbservice.presentToast("Noticia Modificada");
+    this.router.navigate(['/home']);
+  }
 
   ngOnInit() {
   }
-
 }
